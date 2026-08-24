306eec223ccffc0a8a47761cdb18ad545e374584
sahilraj7112-jpg <sahilraj7112@gmail.com>
2026-08-24 17:28:09 +0530

    Pandas file updated

--- DataFrame.py
+++ DataFrame.py
@@ -80,3 +80,16 @@ average_salary = df["Salary"].mean()
 
 print("\nHighest Salary:", highest_salary)
 print("Average Salary:", average_salary)
+
+
+
+import pandas as pd
+
+data = {'Name': ['John', 'Alice', 'Bob', 'Eve', 'Charlie'], 
+        'Age': [25, 30, 22, 35, 28], 
+        'Gender': ['Male', 'Female', 'Male', 'Female', 'Male'], 
+        'Salary': [50000, 55000, 40000, 70000, 48000]}
+
+df = pd.DataFrame(data)
+# Display the entire DataFrame
+print(df)
